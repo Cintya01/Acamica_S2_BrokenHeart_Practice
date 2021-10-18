023bb36fbd74c2caf93e3facd12879596880c618
Cintya_o <85971154+Cintya01@users.noreply.github.com>
2021-10-18 17:36:06 -0300

    Initial commit

---
+++ src/recursos/corazones.js
@@ -0,0 +1,38 @@
+export const corazones = [
+  {
+    id: 1,
+    roto: false
+  },
+  {
+    id: 2,
+    roto: false
+  },
+  {
+    id: 3,
+    roto: true
+  },
+  {
+    id: 4,
+    roto: true
+  },
+  {
+    id: 5,
+    roto: true
+  },
+  {
+    id: 6,
+    roto: true
+  },
+  {
+    id: 7,
+    roto: true
+  },
+  {
+    id: 8,
+    roto: true
+  },
+  {
+    id: 9,
+    roto: false
+  }
+];
